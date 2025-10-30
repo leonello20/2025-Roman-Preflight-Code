fd0c68563d1380d2a8f76d5bd13ac6c9cccc17a1
leonello20 <leonello.castro.cillis@gmail.com>
2025-10-29 21:44:37 -0400

    Update gaussian_occulter.py

--- hcipy_test/gaussian_occulter.py
+++ hcipy_test/gaussian_occulter.py
@@ -4,5 +4,5 @@ def gaussian_occulter_generator(grid,sigma):
     y = grid.y
     r = np.sqrt(x**2 + y**2)
     sigma_val = (r/sigma)
-    transmission_field = 1.0 - 0*np.exp(-0.5 * sigma_val**2)
+    transmission_field = 1.0 - np.exp(-0.5 * sigma_val**2)
     return transmission_field
